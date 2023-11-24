9a228f64766bc5c8acede06d0c741fba4c02355c
Ryan-Zhong <2864103063@qq.com>
2023-11-24 23:07:13 +0800

    chore：增加nodePolyfills插件、移除iife模块替换为umd模块

--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,34 @@ import alias from '@rollup/plugin-alias'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 
-const createOutput = (suffix, format) => ({
-  file: `dist/meiko.${suffix}`,
-  format: format ?? suffix
-})
+const createOutput = (suffix, format, options = {}) => {
+  const obj = {
+    file: `dist/meiko.${suffix}`,
+    format: format ?? suffix
+  }
+  return Object.assign(obj, options)
+}
+
+const outputOptions = {
+  cjs: createOutput('cjs'),
+  mjs: createOutput('mjs', 'esm'),
+  umd: createOutput('umd.js', 'umd', { name: 'Meiko' })
+}
 
 export default {
   input: 'src/index.ts',
-  output: [
-    createOutput('cjs'),
-    createOutput('mjs', 'esm'),
-    Object.assign(createOutput('js', 'iife'), { name: 'Meiko' })
-  ],
+  output: Object.values(outputOptions),
   plugins: [
-    resolve(),
     commonjs(),
-    babel({
-      exclude: 'node_modules/**'
-    }),
+    resolve(),
+    babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
     typescript(),
-    alias({
-      entries: [{ find: '@', replacement: './src' }]
-    }),
-    terser()
+    alias({ entries: [{ find: '@', replacement: './src' }] }),
+    terser(),
+    nodePolyfills()
   ]
 }
