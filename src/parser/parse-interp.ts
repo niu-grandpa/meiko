94adb323b1dc5e6d5e1981bb297c7b14efd31d4c
Ryan-Zhong <2864103063@qq.com>
2023-11-24 14:40:51 +0800

    refactor：重构插值语法解析器

--- src/parser/parse-interp.ts
+++ src/parser/parse-interp.ts
@@ -1,4 +1,4 @@
-import { CompilerContext } from '@/compiler/context'
+import { CompilerContext } from '@/compiler/types'
 import { isEqRSCB, isEqSTN, isEqSpace } from '.'
 
 type ParseInterExprResult = {
@@ -13,35 +13,38 @@ export function parseInterpExpr(
 ): ParseInterExprResult {
   const { source } = context
   const data: ParseInterExprResult = {
-    jump: 0,
+    jump: 1,
     name: '',
     temp: []
   }
 
-  let j = pos + 2
   let interpName = ''
-  let isCancel = false
+  let invalid = false
 
-  // 寻找对应的 '}}' 作为结束条件
-  while (!isEqRSCB(source[j]) && !isEqRSCB(source[j + 1])) {
-    // 1.未匹配到对应 '}}' ，而是匹配到开始标签 '<' ，此次插值处理作废
-    // 2.当匹配到一个 '}' 而下一个非 '}'，则此次处理作废
-    if (
-      isEqSTN(source[j]) ||
-      (isEqRSCB(source[j]) && !isEqRSCB(source[j + 1]))
-    ) {
-      isCancel = true
+  while (true) {
+    data.jump++
+    // 寻找对应的 '}}' 作为结束条件
+    if (isEqRSCB(source[pos]) && isEqRSCB(source[pos + 1])) {
       break
     }
-    if (source[j] || !isEqSpace(source[j])) {
-      interpName += source[j]
+    // 1.未匹配到对应 '}}' ，而是匹配到开始标签 '<' ，此次插值处理作废
+    // 2.当匹配到一个 '}' 而下一个非 '}'，则此次处理作废
+    // invalid: <div>{{xx}</div> or <div>{{xx</div>
+    if (
+      isEqSTN(source[pos]) ||
+      (isEqRSCB(source[pos]) && !isEqRSCB(source[pos + 1]))
+    ) {
+      invalid = true
+      break
     }
-    data.temp.push(source[j++])
+    if (source[pos] || !isEqSpace(source[pos])) {
+      interpName += source[pos]
+    }
+    data.temp.push(source[pos++])
   }
 
-  if (!isCancel) {
+  if (!invalid) {
     // 顺利遇到 }}，由于上面结束条件是预判了下一个 }，因此指针从第一个 } 的位置向后跳2
-    data.jump = j + 2
     data.name = interpName
     data.temp.length = 0
   }
