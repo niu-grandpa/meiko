277702ac77fe1beb670e8cef29712f98c001a6f6
Ryan-Zhong <2864103063@qq.com>
2023-11-24 14:43:13 +0800

    refactor：导出编译器的创建函数

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,16 +1,1 @@
-import { parseHtml } from '@/parser'
-import { CompilerContext, contextCreator } from './context'
-
-type Compiler = (html: string) => CompilerContext
-
-export default function compilerCreator(): Compiler {
-  const context = contextCreator()
-
-  const compilerFn = (html: string): CompilerContext => {
-    context.source = html.trim()
-    parseHtml(context)
-    return context
-  }
-
-  return compilerFn
-}
+export { createCompiler, createContext, createInterpTag } from './creator'
