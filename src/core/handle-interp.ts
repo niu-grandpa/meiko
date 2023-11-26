298a06abdedf4f01e5a21274b83b6b419465bc4d
Ryan-Zhong <2864103063@qq.com>
2023-11-26 16:13:55 +0800

    refactor: 更改插值元素哈希获取

--- src/core/handle-interp.ts
+++ src/core/handle-interp.ts
@@ -1,12 +1,12 @@
 import { CompilerContext } from '@/compiler/types'
-import createHash from '@/helper/hash'
+import getInterpHash from '@/helper/hash'
 import getInterpTag from '@/helper/interp-tag'
 
 export default function handleInterpExpr(
   name: string,
   result: CompilerContext['result']
 ) {
-  const hash = createHash(name)
+  const hash = getInterpHash(name)
   result.push(...getInterpTag(hash))
   // todo 响应式数据关联收集哈希
 }
