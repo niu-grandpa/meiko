aa165919c0b82e2b2a6666d22b952b06aba08a8f
Ryan-Zhong <2864103063@qq.com>
2023-11-28 10:20:50 +0800

    refactor：字符匹配使用charAt，避免undefined

--- src/parser/parse-interp.ts
+++ src/parser/parse-interp.ts
@@ -4,6 +4,7 @@ import { isEqRSCB, isEqSTN, isEqSpace } from '@/helper/equal'
 type ParseInterExprResult = {
   jump: number
   name: string
+  invalid: boolean
   temp: string[]
 }
 
@@ -15,35 +16,34 @@ export default function parseInterpExpr(
   const data: ParseInterExprResult = {
     jump: 1,
     name: '',
+    invalid: false,
     temp: []
   }
 
   let interpName = ''
-  let invalid = false
 
   while (true) {
     data.jump++
+    const ch = source.charAt(pos)
+    const next = source.charAt(pos + 1)
     // 寻找对应的 '}}' 作为结束条件
-    if (isEqRSCB(source[pos]) && isEqRSCB(source[pos + 1])) {
+    if (isEqRSCB(ch) && isEqRSCB(next)) {
       break
     }
     // 1.未匹配到对应 '}}' ，而是匹配到开始标签 '<' ，此次插值处理作废
     // 2.当匹配到一个 '}' 而下一个非 '}'，则此次处理作废
     // invalid: <div>{{xx}</div> or <div>{{xx</div>
-    if (
-      isEqSTN(source[pos]) ||
-      (isEqRSCB(source[pos]) && !isEqRSCB(source[pos + 1]))
-    ) {
-      invalid = true
+    if (isEqSTN(ch) || (isEqRSCB(ch) && !isEqRSCB(next))) {
+      data.invalid = true
       break
     }
-    if (source[pos] || !isEqSpace(source[pos])) {
-      interpName += source[pos]
+    if (ch || !isEqSpace(ch)) {
+      interpName += ch
     }
-    data.temp.push(source[pos++])
+    data.temp.push(source.charAt(pos++))
   }
 
-  if (!invalid) {
+  if (!data.invalid) {
     // 顺利遇到 }}，由于上面结束条件是预判了下一个 }，因此指针从第一个 } 的位置向后跳2
     data.name = interpName
     data.temp.length = 0
