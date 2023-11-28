6154472217649006d6383a4d52f16df35f55bb66
Ryan-Zhong <2864103063@qq.com>
2023-11-28 10:19:30 +0800

    refactor: 当无效解析时，对特殊字符进行转义

--- src/parser/parse-tag.ts
+++ src/parser/parse-tag.ts
@@ -1,9 +1,11 @@
-import { isEqETN, isEqSpace } from '@/helper/equal'
+import { ESCAPE_HTML } from '@/const'
+import { isEqETN, isEqSTN, isEqSpace } from '@/helper/equal'
 
 type ParseTagNameResult = {
   jump: number
   name: string
   stopped: boolean
+  invalid: boolean
 }
 
 export default function parseTagName(
@@ -11,16 +13,26 @@ export default function parseTagName(
   source: string,
   isClosed = false
 ): ParseTagNameResult {
-  const data = { jump: 1, name: '', stopped: false }
+  const data: ParseTagNameResult = {
+    jump: 1,
+    name: '',
+    stopped: false,
+    invalid: false
+  }
 
-  while (!isEqETN(source[pos])) {
+  while (!isEqETN(source.charAt(pos))) {
     data.jump++
-    if (!isEqSpace(source[pos])) {
-      data.name += source[pos++]
-    } else {
+    const ch = source.charAt(pos)
+    if (isEqSTN(ch)) {
+      data.invalid = true
+      data.name += ESCAPE_HTML.gt
+      break
+    }
+    if (isEqSpace(ch)) {
       data.stopped = true
       break
     }
+    data.name += source.charAt(pos++)
   }
   // 当前指针结束于 '>'，因此还要往前移一位
   // 如果是闭合标签，exp: </div>，则指针会停留在 'v' 的位置，因此需要前移两位
