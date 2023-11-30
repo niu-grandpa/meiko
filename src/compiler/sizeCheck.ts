9d0b456fade7afbfa32f89731d23aaebd635e378
Ryan-Zhong <2864103063@qq.com>
2023-11-30 09:59:35 +0800

    fix: 修正计算方法

--- src/compiler/sizeCheck.ts
+++ src/compiler/sizeCheck.ts
@@ -1,17 +1,12 @@
-export default function checkSize(arr: string | string[]): string {
-  const dataTypeSizes: { [key: string]: number | ((value: any) => number) } = {
-    number: 8,
-    string: (value: string) => value.length * 2,
-    object: 32
-  }
-
-  let totalSize = 0
-  for (let i = 0; i < arr.length; i++) {
-    const dataType = typeof arr[i]
-    const size = dataTypeSizes[dataType]
-    totalSize += typeof size === 'number' ? size : size(arr[i])
-  }
-
-  // 将内存占用大小从字节转换为 MB
-  return (totalSize / 1024 ** 2).toFixed(2) + 'MB'
+export default function checkSize(arr: string[]): string {
+  let size: number = 0
+  arr.forEach(item => {
+    if (typeof item === 'string') {
+      size += Buffer.byteLength(item, 'utf8')
+    } else if (typeof item === 'number') {
+      // Assuming each number occupies 8 bytes
+      size += 8
+    }
+  })
+  return (!size ? 0 : (size / 1024).toFixed(2)) + ' kb'
 }
