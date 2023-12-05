e135db8c1dd4a0b934a0710d9adeef12dadf1e91
Ryan-Zhong <2864103063@qq.com>
2023-12-05 12:21:18 +0800

    feat：通用函数等等

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,1 +1,15 @@
-export { createModel } from './creator'
+import { ReactiveFlags } from '@/shared/const'
+
+export interface Target {
+  [ReactiveFlags.SKIP]?: boolean
+  [ReactiveFlags.IS_REACTIVE]?: boolean
+  [ReactiveFlags.IS_READONLY]?: boolean
+  [ReactiveFlags.IS_DEEP]?: boolean
+  [ReactiveFlags.RAW]?: any
+}
+
+export function isReadonly(value: unknown): boolean {
+  return !!(value && (value as Target)[ReactiveFlags.IS_READONLY])
+}
+
+export { createModel } from './createModel'
