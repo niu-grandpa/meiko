a5137bc2491ca6644827a2a957885fc541a71ec8
Ryan-Zhong <2864103063@qq.com>
2023-12-09 11:57:01 +0800

    refactor: 基础proxy处理器

---
+++ src/reactivity/proxy/baseHandler.ts
@@ -0,0 +1,84 @@
+import { defProp } from '@/helper'
+import { isEqual, isFunction, isObject } from '@/helper/equal'
+import { domDepNotify } from '@/reactivity/notify'
+import { ReactiveFlags } from '@/shared/const'
+import {
+  ProxyContext,
+  ProxyGetter,
+  ProxyKey,
+  ProxySetter,
+  Target,
+  createProxy
+} from '.'
+
+export const isReadonly = (value: unknown) => {
+  return !!(value && (value as Target)[ReactiveFlags.IS_READONLY])
+}
+export const initAccessKey = (context: ProxyContext, str: string) => {
+  context.accessKey = `${str}.`
+}
+export const setAccessKey = (context: ProxyContext, str: ProxyKey) => {
+  context.accessKey += `${str.toString()}.`
+}
+
+export function proxyGetter<T extends object>(params: ProxyGetter<T>): any
+export function proxyGetter(params: ProxyGetter<object>) {
+  const { target, prop, receiver, context, modelName, handler } = params
+
+  setAccessKey(context, prop)
+  // get Map、Set、WeakMap and other special types of size
+  if (prop === 'size') {
+    return Reflect.get(target, prop, target)
+  }
+  const value = Reflect.get(target, prop, receiver)
+  if (isObject(value)) {
+    if (context.isDeep) {
+      return createProxy(value, context, handler(modelName, context))
+    }
+    initAccessKey(context, modelName)
+    return value
+  }
+  if (isFunction(value)) {
+    initAccessKey(context, modelName)
+    return function (...args: unknown[]) {
+      return Reflect.apply(value, target, args)
+    }
+  }
+
+  initAccessKey(context, modelName)
+  return value
+}
+
+export function proxySetter<T extends object>(params: ProxySetter<T>): boolean
+export function proxySetter(params: ProxySetter<object>) {
+  const { target, prop, receiver, newValue, context, modelName, handler } =
+    params
+
+  setAccessKey(context, prop)
+
+  const { accessKey } = context
+  const oldValue = Reflect.get(target, prop, receiver)
+  let _newValue = newValue
+
+  if (isObject(oldValue)) {
+    if (!context.isDeep) {
+      console.warn(
+        `[Meiko warn]: without deep observation, sub-objects cannot be set: ${accessKey}`
+      )
+      return false
+    }
+    _newValue = createProxy(oldValue, context, handler(modelName, context))
+  }
+
+  const _accessKey = accessKey.replace(/\.$/, '')
+  initAccessKey(context, modelName)
+  defProp(target, prop, _newValue)
+
+  if (!isEqual(oldValue, _newValue)) {
+    Reflect.set(target, prop, _newValue, receiver)
+    domDepNotify.update(_accessKey, _newValue)
+    return true
+  }
+
+  return false
+}
