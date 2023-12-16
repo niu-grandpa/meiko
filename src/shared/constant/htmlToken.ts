0c7b74f4028a3a2be8151f937d3952dd0803c634
Ryan-Zhong <2864103063@qq.com>
2023-12-16 22:10:23 +0800

    feat: 添加标html字符token枚举常量

---
+++ src/shared/constant/htmlToken.ts
@@ -0,0 +1,21 @@
+export const START_TAG = '<'
+export const END_TAG = '>'
+export const WHITE_SPACE = ' '
+export const CLOSING_SLASH = '/'
+
+/**S 表示 Single, CB 表示 CURLY_BRACES */
+export const LF_SCB = '{'
+/**S 表示 Single, CB 表示 CURLY_BRACES */
+export const RT_SCB = '}'
+/**D 表示 Double, CB 表示 CURLY_BRACES */
+export const LF_DCB = '{{'
+/**D 表示 Double, CB 表示 CURLY_BRACES */
+export const RT_DCB = '}}'
+
+export const enum ESCAPE_HTML {
+  lt = '&lt;', // <
+  gt = '&gt;', // >
+  apo = '&#39;', // '
+  amp = '&amp;', // &
+  quot = '&quot;' // "
+}
