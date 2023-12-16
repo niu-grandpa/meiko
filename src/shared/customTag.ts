7e6cb4b70fcd908ed29399beb0662c9c542f6638
Ryan-Zhong <2864103063@qq.com>
2023-12-16 22:13:41 +0800

    refactor：改变import的导入文件路径

--- src/shared/customTag.ts
+++ src/shared/customTag.ts
@@ -1,13 +1,14 @@
+import { SID_DATASET_KEY } from './constant/datasetKey'
 import {
   CLOSING_SLASH,
   END_TAG,
-  INTERP_HTML_TAGNAME,
   START_TAG,
   WHITE_SPACE
-} from '@/shared/const'
+} from './constant/htmlToken'
+import { INTERP_HTML_TAGNAME } from './constant/tagName'
 
 export default function getInterpTag(hash: string): string[] {
-  const attr = `id="${hash}"`
+  const attr = `${SID_DATASET_KEY}="${hash}"`
   const start = [START_TAG, INTERP_HTML_TAGNAME, WHITE_SPACE, attr, END_TAG]
   const end = [START_TAG, CLOSING_SLASH, INTERP_HTML_TAGNAME, END_TAG]
   return start.concat(end)
