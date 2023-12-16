cc41a7658b04c5b57d37a9265b719c6eca28a386
Ryan-Zhong <2864103063@qq.com>
2023-12-16 22:15:47 +0800

    chore：字符串哈希值生成改用quickHash.js

--- src/helper/hash.ts
+++ src/helper/hash.ts
@@ -1,35 +1,23 @@
-import crypto from 'crypto'
+import quickHash from 'quick-hash'
 
-function interpHashCreator() {
-  const hashMap = new Map<string, string>()
+function hashFactory() {
+  const hashMap: Record<string, string> = {}
 
-  let uid = 0
+  let sid = 0
 
-  const computeHash = (key: string): string => {
-    const md5Hash = crypto.createHash('md5').update(key).digest('hex')
-    const firstFour = md5Hash.slice(0, 4)
-    const middleThree = md5Hash.slice(
-      ~~(md5Hash.length / 2) - 1,
-      ~~(md5Hash.length / 2) + 2
-    )
-    const lastThree = md5Hash.slice(md5Hash.length - 3)
-    return firstFour + middleThree + lastThree
-  }
-
-  const creator = (key: string) => {
-    let k = key
-    if (hashMap.has(key)) {
+  const func = (string: string): string => {
+    let unique = string
+    while (hashMap[unique]) {
       // 同样的键也生成不同的哈希值，这样可以避免一个domid对应多个dom节点，需要遍历
-      k = `${k}_${uid++}`
+      unique = `${unique}_${sid++}`
     }
-    const hash = computeHash(k)
-    hashMap.set(key, hash)
-    return hash
+    const hash = quickHash(unique)
+    return (hashMap[string] = hash)
   }
 
-  return creator
+  return func
 }
 
-const getInterpHash = interpHashCreator()
+const createHashByString = hashFactory()
 
-export default getInterpHash
+export default createHashByString
